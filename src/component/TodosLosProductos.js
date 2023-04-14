9ccb254b535fe8d8aeb3f18e245a78de26d5afd4
Facundo Noya <facunoya@hotmail.com>
2023-04-14 16:59:50 -0300

    agregando user

--- src/component/TodosLosProductos.js
+++ src/component/TodosLosProductos.js
@@ -8,15 +8,29 @@ function TodosLosProductos() {
         fetch('http://localhost:3007/mostrarproductos')
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+
                 setProductos(data)
             })
             .catch(error => console.log(error))
-        console.log("esta linea ", productos)
     }, [productos])
 
+
     return (
-        <h1>Hola</h1>
+        <ul>
+            {productos.length === 0 && <p>Cargando...</p>}
+            {
+                productos.map((producto, i) => {
+                    console.log(producto.imgURL)
+                    return (
+                        <li key={i}>
+                            <h3>{producto.Destinations.name}</h3>
+
+                            <img src={producto.imgURL} alt="avatar" width="150" />
+                        </li>
+                    )
+                })
+            }
+        </ul>
     )
 }
 
--- src/component/TodosLosUsuarios.js
+++ src/component/TodosLosUsuarios.js
@@ -1,20 +1,32 @@
 import { useState, useEffect, useRef } from "react"
 
 function TodosLosUsuarios() {
-
+    const [usuarios, setUsuarios] = useState([])
     useEffect(() => {
 
         fetch('http://localhost:3007/allusers')
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+                setUsuarios(data)
             })
             .catch(error => console.log(error))
-    }, [])
+    }, [usuarios])
 
     return (
-
-        <h1>Hola</h1>
+        <ul>
+            {usuarios.length === 0 && <p>Cargando...</p>}
+            {
+                usuarios.map((usuario, i) => {
+                    return (
+                        <li key={i}>
+                            <h3>{usuario.name}</h3>
+                            <img src={usuario.avatar} alt="avatar" width="150" />
+                        </li>
+                    )
+                })
+            }
+        </ul>
     )
 }
 
