4f07df42882819adbc4bfd2d5084fba93c663fae
Facundo Noya <facunoya@hotmail.com>
2023-04-14 16:10:44 -0300

    probando

--- src/component/TodosLosProductos.js
+++ src/component/TodosLosProductos.js
@@ -1,19 +1,21 @@
 import { useState, useEffect, useRef } from "react"
 
-function TodosLosProductos() {
 
+function TodosLosProductos() {
+    const [productos, setProductos] = useState([])
     useEffect(() => {
 
         fetch('http://localhost:3007/mostrarproductos')
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+                setProductos(data)
             })
             .catch(error => console.log(error))
-    }, [])
+        console.log("esta linea ", productos)
+    }, [productos])
 
     return (
-
         <h1>Hola</h1>
     )
 }
